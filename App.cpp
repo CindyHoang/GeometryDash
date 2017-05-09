954f7249761409996b8b5c3fd9c9ca5a94743145
Richard Ho <thisisrich13@gmail.com>
2017-05-09 12:47:32 -0700

    Add files via upload

--- App.cpp
+++ App.cpp
@@ -18,6 +18,7 @@ App::App(const char* label, int x, int y, int w, int h): GlutApp(label, x, y, w,
     score = 0.0;
     highscore = 0.0;
     delay = 5.0;
+	speed = 1.0;
     
     missileX = 0.74;
     missileY = -0.55;
@@ -47,8 +48,10 @@ void App::loadObstacles() {
 //        obstacles.push_back(new Obstacle(0.7+(0.2*i), -0.5+(0.1*i)));
 //    }
     
-    obstacles.push_back(new Triangle(0.5, -0.5));
-    obstacles.push_back(new Enemy(1.5, -0.5));
+	
+	obstacles.push_back(new Triangle(0.5, -0.5));
+    obstacles.push_back(new Enemy(1.7, -0.5));
+	
 //    obstacles.push_back(new Triangle(1.5, -0.5));
 //    obstacles.push_back(new Triangle(1.6, -0.5));
     for (int i = 0; i < 5; i++) {
@@ -60,14 +63,17 @@ void App::loadObstacles() {
     for (int i = 0; i < 5; i++) {
         obstacles.push_back(new Obstacle(3.35+(0.1*i), -0.5));
     }
-    for (int i = 0; i < 2; i++) {
+	for (int i = 0; i < 3; i++) {
+		obstacles.push_back(new Obstacle(4.2 + (0.1*i), -0.3 + (-0.1*i)));
+	}
+    for (int i = 0; i < 3; i++) {
         obstacles.push_back(new Obstacle(5+(0.1*i), -0.5));
     }
     for (int i = 0; i < 2; i++) {
         obstacles.push_back(new Obstacle(5.5+(0.1*i), -0.35));
     }
     for (int i = 0; i < 5; i++) {
-        obstacles.push_back(new Obstacle(6+(0.1*i), -0.2));
+        obstacles.push_back(new Obstacle(6.0+(0.1*i), -0.2));
     }
     for (int i = 0; i < 2; i++) {
         obstacles.push_back(new Obstacle(6.6+(0.1*i), -0.35));
@@ -75,6 +81,14 @@ void App::loadObstacles() {
     for (int i = 0; i < 2; i++) {
         obstacles.push_back(new Obstacle(6.9+(0.1*i), -0.5));
     }
+	for (int i = 0; i < 2; i++) {
+		obstacles.push_back(new Triangle(7.5 + (0.1*i), -0.5 + (0.1*i)));
+	}
+	obstacles.push_back(new Enemy(7.6, -.5));
+
+	for (int i = 0; i < 2; i++) {
+		obstacles.push_back(new Triangle(7.6+ (0.1*i), -0.4 + (-0.1*i)));
+	}
 //    for (int i = 0; i < 3; i++) {
 //        obstacles.push_back(new Obstacle(4.2+(0.1*i), -0.5+(0.2*i)));
 //        obstacles.push_back(new Obstacle(4.2+(0.2*i), -0.5+(0.2*i)));
@@ -85,8 +99,8 @@ void App::loadObstacles() {
 //    for (int i = 0; i < 5; i++) {
 //        obstacles.push_back(new Obstacle(1.2+(0.1*i), -0.4));
 //    }
-//    powerups.push_back(new Powerup(0.7, -0.4));
-//    powerups.push_back(new Powerup(1.5, -0.2, 1));
+    powerups.push_back(new Powerup(0.8, -0.3, 1));
+    powerups.push_back(new Powerup(1.5, -0.2, 0 ));
 //    for (int i = 0; i < 5; i++) {
 //        obstacles.push_back(new Obstacle(2+(0.1*i), -0.2));
 //    }
@@ -124,12 +138,15 @@ void App::draw() {
     
     // Set Color
     glColor3d(1.0, 1.0, 1.0);
+	
+	
     
     if (welcome != 0) {
         if (gameover) {
             drawString(GLUT_BITMAP_HELVETICA_18, "Gameover :(", -0.175, 0, 0);
             drawString(GLUT_BITMAP_HELVETICA_12, "Press 'r' to restart the game", -0.25, -0.1, 0);
-            
+			drawString(GLUT_BITMAP_HELVETICA_18, "Score: ", -0.175, -0.2, 0);
+			drawString(GLUT_BITMAP_HELVETICA_18, pts, 0.08, -0.2, 0);
             glBindTexture(GL_TEXTURE_2D, wall);
             bg->draw();
             glDisable(GL_TEXTURE_2D);
@@ -146,6 +163,15 @@ void App::draw() {
             sprintf(recordPts, "%.1f", highscore);
     
             p->draw();
+
+			glBegin(GL_LINE);
+			glColor3f(1.0f, 0.0f, 1.0f);
+			glVertex2f(1.0f, 0.0f);
+			glVertex2f(10.0f, 0.0f);
+			glVertex2f(1.0f, -0.5f);
+			glVertex2f(20.0f, -0.5f);
+			glEnd();
+			redraw();
     
             for (int i = 0; i < obstacles.size(); i++) {
                 obstacles[i]->draw();
@@ -159,6 +185,7 @@ void App::draw() {
             for (int i = 0; i < powerups.size(); i++) {
                 powerups[i]->draw();
             }
+			
 
         }
     }
@@ -194,6 +221,7 @@ void App::mouseDown(float x, float y){
         loop = true;
         gameover = false;
         score = 0;
+		speed = 1;
     }
     // Redraw the scene
     redraw();
@@ -239,6 +267,8 @@ void App::keyPress(unsigned char key) {
         loop = true;
         gameover = false;
         score = 0;
+		speed = 1;
+		
     }
     
     if (welcome == 0) {
@@ -257,6 +287,7 @@ void App::keyPress(unsigned char key) {
         gameover = false;
         score = 0;
         highscore = 0;
+		speed = 1;
     }
     redraw();
 }
@@ -264,11 +295,17 @@ void App::keyPress(unsigned char key) {
 void App::idle() {
     if (loop) {
 //        delay += 0.1f;
+	
         float jumpHeight = 0.25f;
-        
+		//teal powerup
+		if (p->getR() == 0.0f && p->getG() == 1.0f && p->getB() == 1.0f) {
+			jumpHeight = 0.45f;
+			delay += 0.1f;
+			
+		}
 //        if (p->getR() == 1) {
 //            jumpHeight = 0.3f;
-//        }
+//       }
 //        else {
 //            jumpHeight = 0.2f;
 //        }
@@ -281,10 +318,11 @@ void App::idle() {
                 gameover = true;
                 break;
             }
-            
+			
             if (obstacles[i]->canFire() && obstacles[i]->getX() <= 0.75) {
                 obstacles[i]->missile = true;
-                missileX -= 0.02f;
+                missileX -= 0.02f * speed;
+				
             }
             
             if (p->contains(missileX, missileY, 0.02f, 0.02f)) {
@@ -307,7 +345,6 @@ void App::idle() {
                     break;
                 }
             }
-            
             if (obstacles[i]->getX()+0.1f < p->getX()) {
                 // If the obstacle has passed the player, we should be able to fall
                 p->shouldLand = true;
@@ -332,26 +369,45 @@ void App::idle() {
                 }
             }
  
-            obstacles[i]->setX(x - 0.0125f);
+            obstacles[i]->setX(x - 0.0125f * speed);
             score += 0.01;
             
             if (x < -1) {
                 // Delete obstacle once it goes off the screen
                 obstacles.erase(obstacles.begin());
             }
+			if (obstacles.begin() == obstacles.end()) {
+				welcome = 1;
+				missileX = 0.74;
+				missileY = -0.55;
+				p->resetColor();
+				obstacles.clear();
+				powerups.clear();
+				loadObstacles();
+				loop = true;
+				gameover = false;
+				speed += 0.25f;
+				
+			}
         }
         
         for (int i = 0; i < powerups.size(); i++) {
             float x = powerups[i]->getX();
             
-            powerups[i]->setX(x - 0.01f);
+            powerups[i]->setX(x - 0.007f*speed);
             
-            if (p->contains(powerups[i]->getX(), powerups[i]->getY(), powerups[i]->getW(), powerups[i]->getH())) {
-                p->setColor(powerups[i]->getR(), powerups[i]->getG(), powerups[i]->getB());
-                powerups.erase(powerups.begin());
-            }
-            
-            if (x < -1) {
+			if (p->contains(powerups[i]->getX(), powerups[i]->getY(), powerups[i]->getW(), powerups[i]->getH())) {
+				p->setColor(powerups[i]->getR(), powerups[i]->getG(), powerups[i]->getB());
+				powerups.erase(powerups.begin());
+
+			}
+			
+			// purple powerup
+			else if (p->getR() == 1.0f && p->getG() == 0.0f && p->getB() == 1.0f) {
+				speed = .75f;
+			}
+			
+            if (x < -1 || x > 10) {
                 powerups.erase(powerups.begin());
             }
             
@@ -373,7 +429,7 @@ void App::idle() {
             if (score > highscore) {
                 highscore = score;
             }
-        }
+		}
         redraw();
     }
 }
